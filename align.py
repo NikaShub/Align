37aac90613e5ab2e56d0078aafcd583812805c34
Nika <nshub24@freeuni.edu.ge>
2026-01-04 17:17:38 +0400

    assignment8

--- align.py
+++ align.py
@@ -84,59 +84,85 @@ def testStrands(score, plusScores, minusScores, strandAligned1, strandAligned2):
 ######################## END OF TEST PART ######################################
 ################################################################################
 
+def solve(strand1, strand2, memo):
+    key = (len(strand1), len(strand2))
+    if key in memo:
+        return memo[key]
+    if len(strand2) == 0:
+        res = (len(strand1) * -2, " " * len(strand1), "2" * len(strand1), strand1, " " * len(strand1))
+        memo[key] = res
+        return res
+    if len(strand1) == 0:
+        res = (len(strand2) * -2, " " * len(strand2), "2" * len(strand2), " " * len(strand2), strand2)
+        memo[key] = res
+        return res
+    scrRec, plusRec, minusRec, firstSRec, secondSRec = solve(strand1[1:], strand2[1:], memo) 
+    if strand1[0] == strand2[0]:
+        scoreNoAction = scrRec + 1
+        plusNoAction = "1" + plusRec
+        minusNoAction = " " + minusRec
+    else:
+        scoreNoAction = scrRec - 1
+        plusNoAction = " " + plusRec
+        minusNoAction = "1" + minusRec 
+
+    scrRec1, plusRec1, minusRec1, firstSRec1, secondSRec1 = solve(strand1[1:], strand2, memo) 
+    scoreSpaceOne = scrRec1 - 2
+    plusSpaceOne = " " + plusRec1
+    minusSpaceOne = "2" + minusRec1
+    firstSSpaceOne = strand1[0] + firstSRec1
+    secondSSpaceOne = " " + secondSRec1
+
+    scrRec2, plusRec2, minusRec2, firstSRec2, secondSRec2 = solve(strand1, strand2[1:], memo) 
+    scoreSpaceTwo = scrRec2 - 2
+    plusSpaceTwo = " " + plusRec2
+    minusSpaceTwo = "2" + minusRec2
+    firstSSpaceTwo = " " + firstSRec2
+    secondSSpaceTwo = strand2[0] + secondSRec2
+
+    if scoreNoAction >= scoreSpaceOne and scoreNoAction >= scoreSpaceTwo:
+        ans = (scoreNoAction, plusNoAction, minusNoAction, strand1[0] + firstSRec, strand2[0] + secondSRec)
+    elif scoreSpaceOne >= scoreSpaceTwo:
+        ans = (scoreSpaceOne, plusSpaceOne, minusSpaceOne, firstSSpaceOne, secondSSpaceOne)
+    else:
+        ans = (scoreSpaceTwo, plusSpaceTwo, minusSpaceTwo, firstSSpaceTwo, secondSSpaceTwo)
+
+    memo[key] = ans
+    return ans
+
+
 
 # Computes the score of the optimal alignment of two DNA strands.
 def findOptimalAlignment(strand1, strand2):
-	# if one of the two strands is empty, then there is only
-	# one possible alignment, and of course it's optimal
-	if len(strand1) == 0: return len(strand2) * -2
-	if len(strand2) == 0: return len(strand1) * -2
-
-	# There's the scenario where the two leading bases of
-	# each strand are forced to align, regardless of whether or not
-	# they actually match.
-	bestWith = findOptimalAlignment(strand1[1:], strand2[1:])
-	if strand1[0] == strand2[0]:
-		return bestWith + 1 # no benefit from making other recursive calls
-
-	best = bestWith - 1
-
-	# It's possible that the leading base of strand1 best
-	# matches not the leading base of strand2, but the one after it.
-	bestWithout = findOptimalAlignment(strand1, strand2[1:])
-	bestWithout -= 2 # penalize for insertion of space
-	if bestWithout > best:
-		best = bestWithout
-
-	# opposite scenario
-	bestWithout = findOptimalAlignment(strand1[1:], strand2)
-	bestWithout -= 2 # penalize for insertion of space
-	if bestWithout > best:
-		best = bestWithout
-
-	return best
+    memo = {}
+    return solve(strand1, strand2, memo)
 
 # Utility function that generates a random DNA string of
 # a random length drawn from the range [minlength, maxlength]
 def generateRandomDNAStrand(minlength, maxlength):
-	assert minlength > 0, \
-	       "Minimum length passed to generateRandomDNAStrand" \
-	       "must be a positive number" # these \'s allow mult-line statements
-	assert maxlength >= minlength, \
-	       "Maximum length passed to generateRandomDNAStrand must be at " \
-	       "as large as the specified minimum length"
-	strand = ""
-	length = random.choice(xrange(minlength, maxlength + 1))
-	bases = ['A', 'T', 'G', 'C']
-	for i in xrange(0, length):
-		strand += random.choice(bases)
-	return strand
+    assert minlength > 0, \
+           "Minimum length passed to generateRandomDNAStrand" \
+           "must be a positive number" # these \'s allow mult-line statements
+    assert maxlength >= minlength, \
+           "Maximum length passed to generateRandomDNAStrand must be at " \
+           "as large as the specified minimum length"
+    strand = ""
+    length = random.choice(range(minlength, maxlength + 1))
+    bases = ['A', 'T', 'G', 'C']
+    for i in range(0, length):
+        strand += random.choice(bases)
+    return strand
 
 # Method that just prints out the supplied alignment score.
 # This is more of a placeholder for what will ultimately
 # print out not only the score but the alignment as well.
-def printAlignment(score, out = sys.stdout):
-	out.write("Optimal alignment score is " + str(score) + "\n")
+def printAlignment(score, plus, minus, s1, s2, out = sys.stdout):
+    out.write("Optimal alignment score is " + str(score) + "\n")
+    out.write("+" + plus + "\n")
+    out.write(" " + s1 + "\n")
+    out.write(" " + s2 + "\n")
+    out.write("-" + minus + "\n")
+
 
 # Unit test main in place to do little more than
 # exercise the above algorithm.  As written, it
@@ -148,21 +174,21 @@ def printAlignment(score, out = sys.stdout):
 # the 10s to 60s and still see everything execute very
 # quickly.
 def main():
-        test(-4,
+    test(-4,
              "  11 1 1 11 ",
              "12  2 2 1  2",
              "G ATCG GCAT ",
              "CAAT GTGAATC")
-	while (True):
-		sys.stdout.write("Generate random DNA strands? ")
-		answer = sys.stdin.readline()
-		if answer == "no\n": break
-		strand1 = generateRandomDNAStrand(8, 10)
-		strand2 = generateRandomDNAStrand(8, 10)
-		sys.stdout.write("Aligning these two strands: " + strand1 + "\n")
-		sys.stdout.write("                            " + strand2 + "\n")
-		alignment = findOptimalAlignment(strand1, strand2)
-		printAlignment(alignment)
-
+    while (True):
+        sys.stdout.write("Generate random DNA strands? ")
+        answer = sys.stdin.readline()
+        if answer == "no\n": break
+        strand1 = generateRandomDNAStrand(8, 10)
+        strand2 = generateRandomDNAStrand(8, 10)
+        sys.stdout.write("Aligning these two strands: " + strand1 + "\n")
+        sys.stdout.write("                            " + strand2 + "\n")
+        score, plus, minus, st1, st2 = findOptimalAlignment(strand1, strand2)
+        printAlignment(score, plus, minus, st1, st2)
+        test(score, plus, minus, st1, st2)
 if __name__ == "__main__":
   main()
